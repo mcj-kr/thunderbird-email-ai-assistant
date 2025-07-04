0b18ef546fc55ef7190beba7fd80b9248e6ce77c
Maciej Kraus <mcj.krs@gmail.com>
2025-07-05 00:26:57 +0200

    tweaks in options

--- build.bash
+++ build.bash
@@ -4,4 +4,4 @@
 ./node_modules/.bin/webpack
 
 # 2. Run web-ext to package the extension
-./node_modules/.bin/web-ext build --overwrite-dest --ignore-files "tmp/" "package.json" "package-lock.json" "webpack.config.js"
+./node_modules/.bin/web-ext build --overwrite-dest --ignore-files "tmp/" "package.json" "package-lock.json" "webpack.config.js" --ignore-files "build.bash" --ignore-files "sign.bash"
